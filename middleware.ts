d1b8746893c6acdb586b8a652b518eb2d56cd4ba
robertandrewhammond-commits <robert.andrew.hammond@gmail.com>
2026-08-31 20:54:43 -0700

    Merge Areas grid into Home, retire standalone Areas page

--- middleware.ts
+++ middleware.ts
@@ -44,7 +44,7 @@ export async function middleware(request: NextRequest) {
 
   if (user && request.nextUrl.pathname === "/login") {
     const url = request.nextUrl.clone();
-    url.pathname = "/areas";
+    url.pathname = "/home";
     return NextResponse.redirect(url);
   }
 
